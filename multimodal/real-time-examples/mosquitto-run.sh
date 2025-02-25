c182102a2067b2fcf988d734e2288e3f16d14dd8
Raymond LeClair <raymond.leclair@springbok.io>
2025-02-25 18:30:48 +0000

    Ignore poetry environment and improve mosquitto run script

--- multimodal/real-time-examples/mosquitto-run.sh
+++ multimodal/real-time-examples/mosquitto-run.sh
@@ -1,5 +1,6 @@
 mkdir -p mosquitto/config
 mkdir -p mosquitto/data
 mkdir -p mosquitto/log
-docker run -it -d -p 1883:1883 --name mos \
+docker run -it -d --name mos  \
+       -p 1883:1883 \
        -v $PWD/mosquitto:/mosquitto eclipse-mosquitto:latest
