ae6c34b0c0c19b578a179cc13abdb4ccaccdd6c9
Raymond LeClair <raymond.leclair@springbok.io>
2025-02-26 14:12:50 +0000

    Clean up shell scripts

--- multimodal/real-time-examples/mosquitto-stop.sh
+++ multimodal/real-time-examples/mosquitto-stop.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 container_id=$(docker ps | grep mosquitto | cut -d " " -f 1)
 if [ -n "$container_id" ]; then
-    pushd paho-mqtt-examples
-    python client_pub_opts.py -H localhost -u mosquitto -p $MOSQUITTO_PASSWD -P 1883
-    popd
+    echo "Stopping mosquitto"
+    docker stop \
+	   $container_id > /dev/null
+    echo "Removing mosquitto"
+    docker rm \
+	   $container_id > /dev/null
 else
     echo "Mosquitto is not running"
 fi
